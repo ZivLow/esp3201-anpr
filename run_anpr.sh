6d1749a3d607217a6fe232e535f24320d4dc8540
ZivLow <zivlowenhao@gmail.com>
2022-11-06 20:17:25 +0800

    update

--- run_anpr.sh
+++ run_anpr.sh
@@ -2,18 +2,18 @@ cd ./anpr
 
 LD_LIBRARY_PATH=../alpr_sdk:$LD_LIBRARY_PATH \
 python3 anpr.py \
---vehicle_detect_method 'haar_cascade' \
---plate_detect_method 'ALPR_SDK' \
+--vehicle_detect_method 'YOLOv5' \
+--plate_detect_method 'YOLOv5' \
 --ocr_detect_method 'EasyOCR' \
---video_filename 'traffic_departing_2' \
+--video_filename 'traffic_both_1' \
 --video_file_directory '/home/ziv/speeding_catcher/traffic_footage/' \
 --output_video False \
 --output_video_directory '/home/ziv/speeding_catcher/output_video_demo/' \
 --output_csv False \
---inference_frame_check_interval 30 \
+--inference_frame_check_interval 10 \
 --tracker_frame_check_interval 1 \
 --speed_limit 2 \
---ppm 25.0 \
+--ppm 80.0 \
 --vehicle_track_threshold 8 \
 --plate_track_threshold 6 \
 --vehicle_confidence_threshold 20.0 \
